8fde474ef147422c7a292907fc3acfd4fb467349
hashchange <heim@zeilenwechsel.de>
2016-07-02 20:49:52 +0200

    Updated dependencies for JS Bin demos

--- demo/amd/jsbin/require-config.js
+++ demo/amd/jsbin/require-config.js
@@ -1,13 +1,13 @@
 requirejs.config( {
 
     paths: {
-        'usertiming': 'https://cdn.rawgit.com/nicjansma/usertiming.js/v0.1.6/src/usertiming',
+        'usertiming': 'https://cdn.rawgit.com/nicjansma/usertiming.js/v0.1.8/src/usertiming',
 
-        'jquery': 'https://code.jquery.com/jquery-1.11.3',
+        'jquery': 'https://code.jquery.com/jquery-1.12.4',
 
         'underscore': 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore',
-        'backbone': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.1/backbone',
-        'marionette': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.marionette/2.4.2/backbone.marionette',
+        'backbone': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone',
+        'marionette': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.marionette/2.4.7/backbone.marionette',
 
         'backbone.declarative.views': 'https://cdn.rawgit.com/hashchange/backbone.declarative.views/2.1.0/dist/amd/backbone.declarative.views',
 
